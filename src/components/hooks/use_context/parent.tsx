e51378c884465e73376cd40a11a5fbd75e5927b0
zpran <zanna@prane.org>
2023-09-01 21:42:00 +0100

    feat: useContent task

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-import { Child1 } from './child_1';
-import { Child2 } from './child_2';
-
+import { useState, createContext } from "react";
+import { Child1 } from "./child_1";
+import { Child2 } from "./child_2";
+export const darkThemeContext = createContext(true);
 export const Parent = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
 
-    const [darkTheme, setDarkTheme] = useState(true)
+  const toggleTheme = () => {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  };
 
-    const toggleTheme = () => {
-        setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    }
-
-    return (
-        <div className='section'>
-            <h2>useContext</h2>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
-        </div>
-
-    )
-}
+  return (
+    <darkThemeContext.Provider value={darkTheme}>
+      <div className="section">
+        <h2>useContext</h2>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <Child1 />
+        <Child2 />
+      </div>
+    </darkThemeContext.Provider>
+  );
+};
