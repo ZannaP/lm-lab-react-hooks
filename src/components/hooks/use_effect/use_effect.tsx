03a066451aa0c188f91c55e387ce28a57d08a97f
zpran <zanna@prane.org>
2023-09-02 12:23:01 +0100

    feat: use Effect task

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,52 @@
 // make an api call using useEffect
+import { useEffect, useState } from "react";
 
-export const APICall = () => {
-
-    // Your code here!
-
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
+export const APICall = () => {
+  const [todo, setTodo] = useState<ToDo>();
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((responce) => responce.json())
+      .then((data) => {
+        setTodo(data);
+      })
+      .catch(console.error);
+  }, []);
+  return (
+    <>
+      <h2>useEffect</h2>
+
+      {todo && <p key={todo.id}>{todo.title}</p>}
+    </>
+  );
+};
+
+// for all todos
+export const APICallAll = () => {
+  const [todo, setTodo] = useState<ToDo[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then((responce) => responce.json())
+      .then((data) => {
+        setTodo(data);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect</h2>
+
+      {todo.length &&
+        todo.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </>
+  );
+};
